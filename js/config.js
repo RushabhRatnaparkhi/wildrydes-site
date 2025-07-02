04c1e47a9a91c19972925f7b123704ed1629a624
Rushabh Ratnaparkhi <120812724+RushabhRatnaparkhi@users.noreply.github.com>
2025-07-02 13:52:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_lzifKwjMn', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '4g37bd0f885cqc0925tt14e1k1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        region: 'ap-south-1', // e.g. us-east-2
+        userPoolClientSecret:'1fih7rc8ovf2ec7no1nvsj6kdlh5iah2ij9eo32jv562vlb5qs9a'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
